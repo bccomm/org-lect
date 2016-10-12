19cc38412791e41119b279d6b8cfb09ca461e703
Sh0e <mano155@gmail.com>
2016-10-12 11:47:24 -0700

    Better documentation and style

--- org-lect.el
+++ org-lect.el
@@ -12,19 +12,19 @@ on track"
 							 (or pom (point)) "PAGES"))))
 	(deadl (org-time-stamp-to-now (format-time-string
 				       (org-time-stamp-format nil t)
-				       (org-get-deadline-time (or pom (point))))))
-	)
-    (if (org-entry-get (or pom (point)) "LECT_EFFORT") (* (/ (float (+ 1 (- totpage curpage )))
-							     (org-lect-get-effort-sum))
-							  (org-lect-get-effort (nth 6 (decode-time))))
-    (/ (float (+ 1 (- totpage curpage))) (float (+ 1 deadl))))
-    )
-  
-  )
+				       (org-get-deadline-time (or pom (point)))))))
+    (if (org-entry-get (or pom (point)) "LECT_EFFORT")
+	(* (/ (float (+ 1 (- totpage curpage)))
+	      (org-lect-get-effort-sum))
+	   (org-lect-get-effort (nth 6 (decode-time))))
+      (/ (float (+ 1 (- totpage curpage))) (float (+ 1 deadl))))))
 
 (defun org-lect-update-today (&optional pom)
+  "Puts the amount of effort needed for the current day into the
+PAGESTODAY property"
   (interactive "P")
-  (org-entry-put (point) "PAGESTODAY" (number-to-string (org-lect-needed-effort-today))))
+  (org-entry-put (point) "PAGESTODAY"
+		 (number-to-string (org-lect-needed-effort-today))))
 
 
 
@@ -53,15 +53,12 @@ org-agenda.el/org-agenda-deadline."
   (let* ((marker (or (org-get-at-bol 'org-marker)
 		     (org-agenda-error)))
 	 (buffer (marker-buffer marker))
-	 (pos (marker-position marker))
-	 )
+	 (pos (marker-position marker)))
     (org-with-remote-undo buffer
       (with-current-buffer buffer
 	(widen)
 	(goto-char pos)
-	(org-lect-update-today)))
-      )
-  )
+	(org-lect-update-today)))))
 
 
 
@@ -71,22 +68,30 @@ org-agenda.el/org-agenda-deadline."
 			      (or pom (point)) "LECT_EFFORT"))))
 
 (defun org-lect-get-effort-sum (&optional pom)
-  "Returns the total amount of effort to invest before
-the deadline"
+  "Returns the total amount of effort to invest before the
+deadline. The units returned are the same as are returned by
+org-lect-get-effort"
   (let* ((deadl (org-time-stamp-to-now (format-time-string
 				       (org-time-stamp-format nil t)
 				       (org-get-deadline-time (or pom (point))))))
 	 (weeks (/ deadl 7))
 	 (days (% deadl 7))
-	 (curday (nth 6 (decode-time)))
+	 (curday (nth 6 (decode-time))) ;; FIXME: respect org-extend-day-until
 	 (k 0))
+
+    ;; days stores the number of days until the deadline that are not
+    ;; within a complete week. These extra days can be counted from
+    ;; the current day. We add these up first, multiplied by the
+    ;; appropriate effort factor
     (while (> days -1) (setf k (+ k (org-lect-get-effort
-				     (% (+ days curday ) 7))))
+				     (% (+ days curday) 7))))
 	   (setf days (- days 1)))
+    ;; weeks stores the number of complete weeks. After the extraneous
+    ;; days have been added above, we simply take the complete weeks
+    ;; and multiply by the effort factors for each day.     
     (unless (< weeks 1) (setf j 6) (while (> j -1)
 				     (setf k (+ k (* weeks (org-lect-get-effort j))))
 					  (setf j (- j 1))))
-    k)
-  )
+    (if (= k 0) 1 k)))
 
 
